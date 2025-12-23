f274981d9ef8280b74bbebf962eff726560a2bd2
Ritu <ritikasinha9958@gmail.com>
2025-12-23 21:45:27 +0530

    updated cred

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDSVBk82e1XdhKMr_WkGwSz7Tp4z2kZU7Y",
-  authDomain: "merry-christmas-3bf52.firebaseapp.com",
-  projectId: "merry-christmas-3bf52",
-  storageBucket: "merry-christmas-3bf52.firebasestorage.app",
-  messagingSenderId: "302313269868",
-  appId: "1:302313269868:web:3418024b6f95007d4d0649",
-  measurementId: "G-H7J0M40FWG"
-};
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+}
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
